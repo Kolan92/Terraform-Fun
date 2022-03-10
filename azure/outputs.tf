f8e0f93d521b7620aec5b77285fbf46eefebdde7
Kolan92 <pawel.krzysztof.kolanowski@gmail.com>
2022-03-10 22:01:46 +0100

    Add azure kubernetes terraform example

---
+++ azure/outputs.tf
@@ -0,0 +1,20 @@
+output "kube_config" {
+  value     = module.cluster.kube_config
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value = module.cluster.cluster_ca_certificate
+}
+
+output "client_certificate" {
+  value = module.cluster.client_certificate
+}
+
+output "client_key" {
+  value = module.cluster.client_key
+}
+
+output "host" {
+  value = module.cluster.host
+}
